44469d36e1ef7d8cdef65c4809734ad8f499abf3
manasadvr <manasadvr@gmail.com>
2020-02-07 13:14:26 +0530

    I have been modified

--- app.py
+++ app.py
@@ -10,34 +10,6 @@ def index():
     return render_template("index.html")
 
 
-app.config.update(
-    DEBUG=True,
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    #MAIL_USERNAME='manasadvr@gmail.com',
-    #MAIL_PASSWORD='Magical@289',
-)
-mail = Mail(app)
-
-
-@app.route('/test_path')
-def test_path():
-    return 'test_path'
-
-
-@app.route('/send-mail')
-def send_mail():
-    try:
-        msg = Message("This is an email that is being sent through flask application", sender="manasadvr@gmail.com",
-                      recipients=["manasadvr@gmail.com"])
-        msg.body = "Yo! This is crazy"
-        mail.send(msg)
-        return "mail sent succesfully"
-    except Exception as e:
-        return str(e)
-
-
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
